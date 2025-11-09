430b8f689c1287a9b0d6e1d0db4254653a6e07c3
phuong nguyen <phuong.nm.de@gmail.com>
2025-11-09 23:34:17 +0100

    rework startup to use constructor injection

--- src/java/ordering-signalrhub/src/main/java/com/eshop/ordering_signalrhub/Startup.java
+++ src/java/ordering-signalrhub/src/main/java/com/eshop/ordering_signalrhub/Startup.java
@@ -1,32 +1,42 @@
 package com.eshop.ordering_signalrhub;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.eshop.eventbus.EventBus;
 import com.eshop.ordering_signalrhub.events.OrderStatusChangedToAwaitingValidationIntegrationEvent;
 import com.eshop.ordering_signalrhub.events.OrderStatusChangedToAwaitingValidationIntegrationEventHandler;
+import com.eshop.ordering_signalrhub.events.OrderStatusChangedToCancelledIntegrationEvent;
+import com.eshop.ordering_signalrhub.events.OrderStatusChangedToCancelledIntegrationEventHandler;
+import com.eshop.ordering_signalrhub.events.OrderStatusChangedToPaidIntegrationEvent;
+import com.eshop.ordering_signalrhub.events.OrderStatusChangedToPaidIntegrationEventHandler;
+import com.eshop.ordering_signalrhub.events.OrderStatusChangedToShippedIntegrationEvent;
+import com.eshop.ordering_signalrhub.events.OrderStatusChangedToShippedIntegrationEventHandler;
+import com.eshop.ordering_signalrhub.events.OrderStatusChangedToStockConfirmedIntegrationEvent;
+import com.eshop.ordering_signalrhub.events.OrderStatusChangedToStockConfirmedIntegrationEventHandler;
+import com.eshop.ordering_signalrhub.events.OrderStatusChangedToSubmittedIntegrationEvent;
+import com.eshop.ordering_signalrhub.events.OrderStatusChangedToSubmittedIntegrationEventHandler;
 
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-
-// configure service: connectionFactory
-// registerEventBus
-//      - create eventBusRabbitMq
-// configureEventBus
-//      - subscribe event and event handler
 
 @Slf4j
 @Component
 public class Startup {
 
-    // Let creation of eventBus take place before @PostConstruct
-    @Autowired
-    private EventBus eventBus;
+    private final EventBus eventBus;
+
+    public Startup(EventBus eventBus) {
+        this.eventBus = eventBus;
+    }
 
     @PostConstruct
-    private void postConstruct() {
+    private void configureEventBus() {
         log.info("Startup postConstruct");
         eventBus.subscribe(OrderStatusChangedToAwaitingValidationIntegrationEvent.class, OrderStatusChangedToAwaitingValidationIntegrationEventHandler.class);
+        eventBus.subscribe(OrderStatusChangedToCancelledIntegrationEvent.class, OrderStatusChangedToCancelledIntegrationEventHandler.class);
+        eventBus.subscribe(OrderStatusChangedToPaidIntegrationEvent.class, OrderStatusChangedToPaidIntegrationEventHandler.class);
+        eventBus.subscribe(OrderStatusChangedToShippedIntegrationEvent.class, OrderStatusChangedToShippedIntegrationEventHandler.class);
+        eventBus.subscribe(OrderStatusChangedToStockConfirmedIntegrationEvent.class, OrderStatusChangedToStockConfirmedIntegrationEventHandler.class);
+        eventBus.subscribe(OrderStatusChangedToSubmittedIntegrationEvent.class, OrderStatusChangedToSubmittedIntegrationEventHandler.class);
     }
 }
