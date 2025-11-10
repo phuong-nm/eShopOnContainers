bdacac2b0d00818974184cd5780d3d6ac04d10ae
phuong nguyen <phuong.nm.de@gmail.com>
2025-11-10 13:56:10 +0100

    refactor ordering-signalrhub to use eventbus-rabbitmq dependency

--- src/java/ordering-signalrhub/src/main/java/com/eshop/ordering_signalrhub/EventBusFactory.java
+++ src/java/ordering-signalrhub/src/main/java/com/eshop/ordering_signalrhub/EventBusFactory.java
@@ -1,12 +1,13 @@
 package com.eshop.ordering_signalrhub;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import com.eshop.eventbus.EventBus;
 import com.eshop.eventbus.EventBusSubscriptionManager;
+import com.eshop.eventbus_rabbitmq.EventBusRabbitMq;
+
 import org.springframework.amqp.rabbit.connection.Connection;
 
 import lombok.extern.slf4j.Slf4j;
