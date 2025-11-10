b636425113a6ea18af627b0533636e98cd54550b
Phuong Nguyen <phuong.nm.de@gmail.com>
2025-11-11 00:50:12 +0100

    update event handler of payment-api

--- src/java/payment-api/src/main/java/com/eshop/payment_api/events/OrderStatusChangedToStockConfirmedIntegrationEventHandler.java
+++ src/java/payment-api/src/main/java/com/eshop/payment_api/events/OrderStatusChangedToStockConfirmedIntegrationEventHandler.java
@@ -1,17 +1,36 @@
 package com.eshop.payment_api.events;
 
+import org.springframework.stereotype.Component;
+
+import com.eshop.eventbus.EventBus;
+import com.eshop.eventbus.IntegrationEvent;
 import com.eshop.eventbus.IntegrationEventHandler;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class OrderStatusChangedToStockConfirmedIntegrationEventHandler
-    implements IntegrationEventHandler<OrderStatusChangedToStockConfirmedIntegrationEvent> {
+@Component
+public class OrderStatusChangedToStockConfirmedIntegrationEventHandler implements IntegrationEventHandler<OrderStatusChangedToStockConfirmedIntegrationEvent> {
+
+    private final EventBus eventBus;
+
+    public OrderStatusChangedToStockConfirmedIntegrationEventHandler(EventBus eventBus) {
+        this.eventBus = eventBus;
+    }
 
     @Override
     public Runnable handle(OrderStatusChangedToStockConfirmedIntegrationEvent event) {
+        log.info("OrderStatusChangedToStockConfirmedIntegrationEventHandler.handle");
         Runnable runnable = () -> {
-            log.info("OrderStatusChangedToStockConfirmedIntegrationEventHandler.handle");
+            boolean paymentSucceeded = true;
+            IntegrationEvent integrationEvent;
+
+            if (paymentSucceeded) {
+                integrationEvent = new OrderPaymentSucceededIntegrationEvent(event.getOrderId());
+            } else {
+                integrationEvent = new OrderPaymentFailedIntegrationEvent(event.getOrderId());
+            }
+            eventBus.publish(integrationEvent);
         };
         return runnable;
     }
