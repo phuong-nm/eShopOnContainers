1ca945c19bf4d2a239d449fd71b028e82aa5b622
phuong nguyen <phuong.nm.de@gmail.com>
2025-11-10 14:49:07 +0100

    fixed handling of unknown properties in deserializing json object value

--- src/java/ordering-signalrhub/src/main/java/com/eshop/ordering_signalrhub/events/OrderStatusChangedToPaidIntegrationEvent.java
+++ src/java/ordering-signalrhub/src/main/java/com/eshop/ordering_signalrhub/events/OrderStatusChangedToPaidIntegrationEvent.java
@@ -1,38 +1,19 @@
 package com.eshop.ordering_signalrhub.events;
 
-import java.util.List;
-
 import com.eshop.eventbus.IntegrationEvent;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonFormat(with = { JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES })
 public class OrderStatusChangedToPaidIntegrationEvent extends IntegrationEvent {
     private int orderId;
     private String orderStatus;
     private String buyerName;
-    private List<OrderStockItem> orderStockItems;
-
-    public OrderStatusChangedToPaidIntegrationEvent(int orderId, String orderStatus, String buyerName, List<OrderStockItem> orderStockItems) {
-        this.orderId = orderId;
-        this.orderStatus = orderStatus;
-        this.buyerName = buyerName;
-        this.orderStockItems = orderStockItems;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public String getOrderStatus() {
-        return orderStatus;
-    }
-
-    public String getBuyerName() {
-        return buyerName;
-    }
-
-    public List<OrderStockItem> getOrderStockItems() {
-        return orderStockItems;
-    }
 }
 
--- src/java/ordering-signalrhub/src/main/java/com/eshop/ordering_signalrhub/events/OrderStatusChangedToStockConfirmedIntegrationEvent.java
+++ src/java/ordering-signalrhub/src/main/java/com/eshop/ordering_signalrhub/events/OrderStatusChangedToStockConfirmedIntegrationEvent.java
@@ -1,31 +1,18 @@
 package com.eshop.ordering_signalrhub.events;
 
-import java.util.List;
-
 import com.eshop.eventbus.IntegrationEvent;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonFormat(with = { JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES })
 public class OrderStatusChangedToStockConfirmedIntegrationEvent extends IntegrationEvent {
     private int orderId;
     private String orderStatus;
     private String buyerName;
-
-    public OrderStatusChangedToStockConfirmedIntegrationEvent(int orderId, String orderStatus, String buyerName, List<OrderStockItem> orderStockItems) {
-        this.orderId = orderId;
-        this.orderStatus = orderStatus;
-        this.buyerName = buyerName;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public String getOrderStatus() {
-        return orderStatus;
-    }
-
-    public String getBuyerName() {
-        return buyerName;
-    }
 }
