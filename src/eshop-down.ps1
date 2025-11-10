64fa2213b9eae9c298a38d601038600e05f7dc40
phuong nguyen <phuong.nm.de@gmail.com>
2025-11-10 13:12:51 +0100

    add project-name eshop to deployment scripts

--- src/eshop-down.ps1
+++ src/eshop-down.ps1
@@ -1,2 +1,2 @@
-docker compose -f docker-compose.yml -f docker-compose.override.yml down -v
+docker compose --project-name eshop -f docker-compose.yml -f docker-compose.override.yml down -v
 docker compose --project-directory ..\deploy -f docker-compose.elk.yml down -v
--- src/eshop-up.ps1
+++ src/eshop-up.ps1
@@ -1,4 +1,4 @@
-docker compose -f docker-compose.yml -f docker-compose.override.yml up --build -d `
+docker compose --project-name eshop -f docker-compose.yml -f docker-compose.override.yml up --build -d `
     webspa webstatus `
     ordering-backgroundtasks `
     ordering-signalrhub `
