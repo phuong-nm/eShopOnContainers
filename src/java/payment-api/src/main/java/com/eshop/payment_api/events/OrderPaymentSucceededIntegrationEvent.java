9e427081e5db2dad1cac427486ae409a50ebc070
phuong nguyen <phuong.nm.de@gmail.com>
2025-11-10 14:44:29 +0100

    fixed handling of missing constructor

--- src/java/payment-api/src/main/java/com/eshop/payment_api/events/OrderPaymentSucceededIntegrationEvent.java
+++ src/java/payment-api/src/main/java/com/eshop/payment_api/events/OrderPaymentSucceededIntegrationEvent.java
@@ -3,16 +3,14 @@ package com.eshop.payment_api.events;
 import com.eshop.eventbus.IntegrationEvent;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonFormat(with = { JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES })
 public class OrderPaymentSucceededIntegrationEvent extends IntegrationEvent {
     private int orderId;
-
-
-    public OrderPaymentSucceededIntegrationEvent(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
 }
