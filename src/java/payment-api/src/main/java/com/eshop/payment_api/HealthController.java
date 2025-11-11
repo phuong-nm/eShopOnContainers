7fcf29b0290a7ad19a40c9feaad1be5ead8824af
phuong nguyen <phuong.nm.de@gmail.com>
2025-11-11 22:10:54 +0100

    add dummy health check endpoints to catalog and payment

---
+++ src/java/payment-api/src/main/java/com/eshop/payment_api/HealthController.java
@@ -0,0 +1,30 @@
+package com.eshop.payment_api;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/hc")
+    public String getHealth() {
+        return new String("{\r\n" + //
+                        "    \"status\": \"Healthy\",\r\n" + //
+                        "    \"totalDuration\": \"00:00:00.0022111\",\r\n" + //
+                        "    \"entries\": {\r\n" + //
+                        "        \"self\": {\r\n" + //
+                        "            \"data\": {},\r\n" + //
+                        "            \"duration\": \"00:00:00.0000060\",\r\n" + //
+                        "            \"status\": \"Healthy\",\r\n" + //
+                        "            \"tags\": []\r\n" + //
+                        "        }\r\n" + //
+                        "    }\r\n" + //
+                        "}");
+    }
+
+    @GetMapping("/liveness")
+    public String getLiveness() {
+        return new String("Healthy");
+    }
+}
