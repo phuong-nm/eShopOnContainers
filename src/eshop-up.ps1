13e300dec7e81ecd8f4d630a705bedd6040ac776
phuong nguyen <phuong.nm.de@gmail.com>
2025-11-11 11:09:09 +0100

    add initial payment-spring-api service

--- src/eshop-up.ps1
+++ src/eshop-up.ps1
@@ -2,6 +2,6 @@ docker compose --project-name eshop -f docker-compose.yml -f docker-compose.over
     webspa webstatus `
     ordering-backgroundtasks `
     ordering-signalrhub `
-    payment-api `
+    payment-spring-api `
     seq
 docker compose --project-directory ..\deploy -f docker-compose.elk.yml up --build -d
